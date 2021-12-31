34b0a8c51599cd9fa1e749694eb3899be01fbec5
Josh Jones <ohblonddev@gmail.com>
2021-12-31 21:11:22 +0000

    Solve day 6 part 2

--- src/domain/solution_executor/day_6.rs
+++ src/domain/solution_executor/day_6.rs
@@ -1,28 +1,39 @@
 use crate::domain::solution_executor::SolutionExecutor;
+use std::collections::HashMap;
 
-#[derive(Debug, derive_new::new)]
+#[derive(Debug)]
 struct DayState {
-    timers: Vec<u8>,
+    timers: HashMap<u8, u64>,
 }
 
 impl DayState {
-    fn tick_day(&mut self) {
-        let mut new_fish_count = 0;
-        self.timers.iter_mut().for_each(|timer| {
-            if *timer == 0 {
-                new_fish_count += 1;
-                *timer = 8;
-            } else {
-                *timer -= 1;
-            }
+    fn new(timers: Vec<u8>) -> Self {
+        let mut map = HashMap::new();
+        timers.into_iter().for_each(|timer| {
+            let count = map.entry(timer).or_insert(0);
+            *count += 1;
         });
-
-        self.timers
-            .append(&mut (0..new_fish_count).map(|_| 6).collect::<Vec<u8>>());
+        DayState { timers: map }
     }
 
-    fn fish_count(&self) -> usize {
-        self.timers.len()
+    fn tick_day(&mut self) {
+        let old_map = self.timers.clone();
+        self.timers.insert(8, *old_map.get(&0).unwrap_or(&0));
+        self.timers.insert(7, *old_map.get(&8).unwrap_or(&0));
+        self.timers.insert(
+            6,
+            *old_map.get(&7).unwrap_or(&0) + *old_map.get(&0).unwrap_or(&0),
+        );
+        self.timers.insert(5, *old_map.get(&6).unwrap_or(&0));
+        self.timers.insert(4, *old_map.get(&5).unwrap_or(&0));
+        self.timers.insert(3, *old_map.get(&4).unwrap_or(&0));
+        self.timers.insert(2, *old_map.get(&3).unwrap_or(&0));
+        self.timers.insert(1, *old_map.get(&2).unwrap_or(&0));
+        self.timers.insert(0, *old_map.get(&1).unwrap_or(&0));
+    }
+
+    fn fish_count(&self) -> u64 {
+        self.timers.values().sum()
     }
 }
 
@@ -31,8 +42,8 @@ pub(crate) struct Day6SolutionExecutor;
 
 impl SolutionExecutor for Day6SolutionExecutor {
     type Input = Vec<u8>;
-    type Part1Output = usize;
-    type Part2Output = usize;
+    type Part1Output = u64;
+    type Part2Output = u64;
 
     fn part_1(&self, input: Self::Input) -> Self::Part1Output {
         let mut state = DayState::new(input);
@@ -40,8 +51,10 @@ impl SolutionExecutor for Day6SolutionExecutor {
         state.fish_count()
     }
 
-    fn part_2(&self, _input: Self::Input) -> Self::Part2Output {
-        unimplemented!()
+    fn part_2(&self, input: Self::Input) -> Self::Part2Output {
+        let mut state = DayState::new(input);
+        (0..256).for_each(|_day| state.tick_day());
+        state.fish_count()
     }
 }
 
@@ -58,5 +71,10 @@ mod tests {
     #[test]
     fn counts_number_of_fish_after_80_days() {
         assert_that(&Day6SolutionExecutor::new().part_1(test_data())).is_equal_to(5934);
+    }
+
+    #[test]
+    fn counts_number_of_fish_after_256_days() {
+        assert_that(&Day6SolutionExecutor::new().part_2(test_data())).is_equal_to(26984457539);
     }
 }
--- tests/challenges/day_06.rs
+++ tests/challenges/day_06.rs
@@ -4,3 +4,8 @@ use crate::helpers::assert_challenge_result;
 fn part_1() {
     assert_challenge_result(6, 1, "386755")
 }
+
+#[test]
+fn part_2() {
+    assert_challenge_result(6, 2, "1732731810807")
+}
